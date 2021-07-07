de44ea94184915c43527201aff9c6e5a423a85a2
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-07-07 19:45:55 +0200

    chore: Change deployment type.

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,6 +1,8 @@
 // pages/api/auth/forgot
 import { createContact } from "../../lib/sendinblue"
 
+console.log("SENDINBLUEKEY:", process.env.SENDINBLUEKEY);
+
 async function handler(req, res) {
     await createContact(req.body.email, {} , req.body.listIds)
     return res.json({ ok: true });
